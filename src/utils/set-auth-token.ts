dd66df47b0070a8bb8d9a4aa89465d3b3d88ee67
Srinivas Akella <akellasrinivas9944@gmail.com>
2025-01-04 01:02:33 +0530

    asf

--- src/utils/set-auth-token.ts
+++ src/utils/set-auth-token.ts
@@ -2,9 +2,6 @@ import { Api } from "../redux/api";
 
 const SetAuthToken = (token: string) => {
 	if (token) {
-		if (!localStorage.getItem("asp-todo-qa-token")) {
-			localStorage.setItem("asp-todo-qa-token", token);
-		}
 		Api.defaults.headers.common["Authorization"] = "Bearer " + token;
 	} else {
 		delete Api.defaults.headers.common["Authorization"];
