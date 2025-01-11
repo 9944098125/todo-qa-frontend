aff1ee4aac470093c324cd40b7a911598439273f
Srinivas Akella <akellasrinivas9944@gmail.com>
2025-01-11 18:59:31 +0530

    dev

--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -1,5 +1,4 @@
 import Axios from "axios";
-import Cookies from "js-cookie";
 
 export const Api = Axios.create({
 	// baseURL: "http://localhost:5000/api",
@@ -8,13 +7,4 @@ export const Api = Axios.create({
 		"Content-Type": "application/json",
 	},
 	withCredentials: true,
-});
-
-// Add a request interceptor to set the Authorization header
-Api.interceptors.request.use((config) => {
-	const token = Cookies.get("asp-todo-qa-token");
-	if (token) {
-		config.headers["Authorization"] = `Bearer ${token}`;
-	}
-	return config;
 });
