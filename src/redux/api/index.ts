1ddc6467a89364b399bcefe76c44deebd85bdad0
Akella Srinivas Prasad <srini@Akellas-Mac-mini.local>
2025-10-25 19:08:18 +0530

    working now

--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -4,7 +4,7 @@ export const Api = Axios.create({
 	// baseURL: "http://localhost:5000/api",
 	// Use full URL including protocol for deployed backend. Prefer reading from an env var
 	// e.g. process.env.REACT_APP_API_BASE_URL
-	baseURL: process.env.REACT_APP_API_URL,
+	baseURL: "https://todo-qa-with-ts-backend-production.up.railway.app/api",
 	headers: {
 		"Content-Type": "application/json",
 	},
