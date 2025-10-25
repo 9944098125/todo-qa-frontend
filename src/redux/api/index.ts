7685a11da77e3f9a684b29f3356679e2fcd62a9c
Srinivas Akella <87494733+9944098125@users.noreply.github.com>
2025-10-25 18:40:41 +0530

    Update API base URL to remove protocol

--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 export const Api = Axios.create({
 	// baseURL: "http://localhost:5000/api",
-	baseURL: "https://todo-qa-with-ts-backend-production.up.railway.app/api",
+	baseURL: "todo-qa-with-ts-backend-production.up.railway.app/api",
 	headers: {
 		"Content-Type": "application/json",
 	},
